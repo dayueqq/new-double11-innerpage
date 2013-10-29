3fe5d1a01ed5b010b223ebcc99aba4f7475c7d62
imGiantChan <530000693@qq.com>
2013-10-29 13:16:14 +0800

    innerpage complete

--- res/js/innerpage.js
+++ res/js/innerpage.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+//右侧边栏浮动效果
+function aa(strId){
+var obj=document.getElementById(strId);
+obj.style.visibility="visible";
+}
+function bb(strId){
+var obj=document.getElementById(strId);
+obj.style.visibility="hidden";
+}
